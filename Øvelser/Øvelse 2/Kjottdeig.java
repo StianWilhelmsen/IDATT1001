96a87882c7cad0d726d8683d5c2d180e4fa9f528
Stian Wilhelmsen <89775276+StianWilhelmsen@users.noreply.github.com>
2021-09-27 11:42:11 +0200

    Add files via upload

---
+++ Øvelser/Øvelse 2/Kjottdeig.java
@@ -0,0 +1,14 @@
+class Kjottdeig {
+	public static void main (String [] args) {
+		double kjottA = (35.9/450)*1000;
+		double kjottB = (39.5/500)*1000;
+
+		if (kjottA > kjottB) {
+			System.out.println("Kjottdeig B er billigere enn kjottdeig A.");
+		}
+
+		else {
+			System.out.println("Kjottdeig A er billigere enn kjottdeig B.");
+		}
+	}
+}
